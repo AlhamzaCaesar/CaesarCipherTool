bfbd7ee29677b0b91efc07129ca48f9d38efe320
AlhamzaCaesar <72037456+AlhamzaCaesar@users.noreply.github.com>
2025-11-10 00:28:19 +0300

    Cipher.py

--- Cipher.py
+++ Cipher.py
@@ -2,12 +2,16 @@ import string
 alphabet = string.ascii_lowercase + string.ascii_lowercase
 print("𝓐𝓵𝓱𝓪𝓶𝔃𝓪  𝓒𝓪𝓮𝓼𝓪𝓻 𝓒𝓲𝓹𝓱𝓮𝓻 𝓽𝓸𝓸𝓵")
 input("Press Enter To Start ! ")
-word = input("Enter the word you want to encrypt : ").lower()
+word = input("Enter the type you want to encrypt : ").lower()
 key =int(input("Enter the key :"))
 encrypted_word = ""
-for letter in word :
-    original_position = alphabet.index(letter)
-    new_position = original_position+key
-    encrypted_word += alphabet[new_position]
 
+for letter in word :
+    if letter in alphabet :
+        original_position = alphabet.index(letter)
+        new_position = original_position+key
+        encrypted_word += alphabet[new_position]
+    else :
+        encrypted_word+=letter
+        
 print(f"The encrypted word : {encrypted_word}")
