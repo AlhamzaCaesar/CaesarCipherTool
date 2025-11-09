db67ee4077acc8d92b9b536df45e3b2b78147d3e
AlhamzaCaesar <72037456+AlhamzaCaesar@users.noreply.github.com>
2025-11-10 00:17:37 +0300

    Cipher.py

---
+++ Cipher.py
@@ -0,0 +1,13 @@
+import string
+alphabet = string.ascii_lowercase + string.ascii_lowercase
+print("𝓐𝓵𝓱𝓪𝓶𝔃𝓪  𝓒𝓪𝓮𝓼𝓪𝓻 𝓒𝓲𝓹𝓱𝓮𝓻 𝓽𝓸𝓸𝓵")
+input("Press Enter To Start ! ")
+word = input("Enter the type you want to encrypt : ").lower()
+key =int(input("Enter the key :"))
+encrypted_word = ""
+for letter in word :
+    original_position = alphabet.index(letter)
+    new_position = original_position+key
+    encrypted_word += alphabet[new_position]
+
+print(f"The encrypted word : {encrypted_word}")
